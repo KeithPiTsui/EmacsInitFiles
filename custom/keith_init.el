10d4f4364781b3a3d682a2c8b804c1102b53b39a
k <k@ks-MacBook-Pro.local>
2019-04-01 16:03:30 +0800

    emacs http proxy

--- custom/keith_init.el
+++ custom/keith_init.el
@@ -8,3 +8,30 @@
 ;;; Show column and row number in mode line)
 (setq column-number-mode t)
 (setq global-linum-mode t)
+
+;;; Setup frame size.
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+      (progn
+        ;; use 120 char wide window for largeish displays
+        ;; and smaller 80 column windows for smaller displays
+        ;; pick whatever numbers make sense for you
+        (if (> (x-display-pixel-width) 1280)
+            (add-to-list 'default-frame-alist (cons 'width 120))
+          (add-to-list 'default-frame-alist (cons 'width 80)))
+        ;; for the height, subtract a couple hundred pixels
+        ;; from the screen height (for panels, menubars and
+        ;; whatnot), then divide by the height of a char to
+        ;; get the height we want
+        (add-to-list 'default-frame-alist
+                     (cons 'height (/ (- (x-display-pixel-height) 200)
+                                      (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
+;;; Setup http proxy
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+        ("http" . "127.0.0.1:1087")
+        ("https" . "127.0.0.1:1087")))
